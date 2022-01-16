58e2d5ba7375af329e7e94cd92fd5a76c4a07539
Rachel-jieun Jeong <stopluna@naver.com>
2022-01-17 01:07:48 +0900

    커밋 테스트

--- board/src/main/java/com/mysite/controller/BoardController.java
+++ board/src/main/java/com/mysite/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.mysite.service.BoardServiceImple;
 import com.mysite.vo.BoardVo;
@@ -30,6 +31,24 @@ public class BoardController {
 		BoardVo board = new BoardVo(request.getParameter("title"),request.getParameter("writer"),request.getParameter("content"),request.getFile("fileName").getOriginalFilename().replace(" ", ""));
 		System.out.println(board.toString());
 		return "board/boardList";
+	}
+	
+	@RequestMapping(value="/board")
+	public ModelAndView boardRead(MultipartHttpServletRequest request) {
+		logger.info("게시글 상세 페이지 요청");
+		ModelAndView view = new ModelAndView();
+		view.setViewName("board/boardRead");
+		
+		return view;
+	}
+	
+	@RequestMapping(value="/boardUp")
+	public ModelAndView boardUpdate() {
+		logger.info("게시글 수정 요청");
+		ModelAndView view = new ModelAndView();
+		view.setViewName("redirect:/board/boardRead?bo=");
+		
+		return view;
 	}
 
 }
