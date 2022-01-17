a1d66dec6862260dfb4e5840a2e6d9ba6e7206cd
Rachel-jieun Jeong <stopluna@naver.com>
2022-01-18 01:28:58 +0900

    엑셀 파일 저장 성공!

--- board/src/main/java/com/mysite/controller/ExcelDownloadController.java
+++ board/src/main/java/com/mysite/controller/ExcelDownloadController.java
@@ -10,12 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.poi.hssf.util.HSSFColor.HSSFColorPredefined;
 import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
-import org.apache.poi.xssf.streaming.SXSSFCell;
-import org.apache.poi.xssf.streaming.SXSSFRow;
-import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,7 +34,6 @@ public class ExcelDownloadController {
 	private MemberServiceImple memberService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);	
-	private static final String filePath = "C://MemberFile";
 	private static int excelNum = 1;
 	
 	@RequestMapping(value="/downDoc")
@@ -52,13 +52,13 @@ public class ExcelDownloadController {
 		Date todayTime = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
 		String str = sdf.format(todayTime);
-       	String fileName = "result_" + str + ".xls";
+       	String fileName = "result_" + str + ".xlsx";
        	
 		//Workbook workbook = new HSSFWorkbook();	//Excel 97 ~ 2003 버전인 경우
-		//XSSFWorkbook workbook = new XSSFWorkbook();	//Excel 2007 이상인 경우
-		 SXSSFWorkbook workbook = new SXSSFWorkbook();		//Excel 2007 이상, 대용량 Excel 처리에 적합하며 쓰기 전용.
+		//Workbook workbook = new XSSFWorkbook();	//Excel 2007 이상인 경우
+		 Workbook workbook = new SXSSFWorkbook();		//Excel 2007 이상, 대용량 Excel 처리에 적합하며 쓰기 전용.
 	         
-		 SXSSFSheet sheet = workbook.createSheet("MEMBERLIST");
+		 Sheet sheet = workbook.createSheet("MEMBERLIST");
 	         
 	        Iterator<MemberVo> iterator = memberList.iterator();
 	        
@@ -83,69 +83,67 @@ public class ExcelDownloadController {
 	        bodyStyle.setBorderRight(BorderStyle.THIN);
 	         
 	        int rowIndex = 0;
-	        int excelname=0; // 처음에는 ID 학번등 고정값을 넣기 위해 사용한 변수
-	        do{
-	        	MemberVo member = iterator.next();
-	        	SXSSFRow row = sheet.createRow(rowIndex++);
-	           
-	            
-	            if(excelname==0){ // 처음에 고정값 
-	                 
-	            	SXSSFCell cell0 = row.createCell(0);
-	            	cell0.setCellStyle(headStyle);
-	                cell0.setCellValue("No");
-	                SXSSFCell cell1 = row.createCell(1);
-	            	cell1.setCellStyle(headStyle);
-	                cell1.setCellValue("직원번호");
-	                SXSSFCell cell2 = row.createCell(2);
-	            	cell2.setCellStyle(headStyle);
-	                cell2.setCellValue("이름");
-	                SXSSFCell cell3 = row.createCell(3);
-	            	cell3.setCellStyle(headStyle);
-	                cell3.setCellValue("전화번호");
-	                SXSSFCell cell4 = row.createCell(4);
-	            	cell4.setCellStyle(headStyle);
-	                cell4.setCellValue("직급");
-	                SXSSFCell cell5 = row.createCell(5);
-	            	cell5.setCellStyle(headStyle);
-	                cell5.setCellValue("이메일");
-	                excelname++;
-	                
-	            }else{  // 다음부터는 순차적으로 값이 들어감 
-	                 
-	            	SXSSFCell cell0 = row.createCell(0);
-	            	cell0.setCellStyle(bodyStyle);
-	                cell0.setCellValue(excelNum++);
-	                SXSSFCell cell1 = row.createCell(1);
-	            	cell1.setCellStyle(bodyStyle);
-	                cell1.setCellValue(member.getNum());
-	                SXSSFCell cell2 = row.createCell(2);
-	            	cell2.setCellStyle(bodyStyle);
-	                cell2.setCellValue(member.getName());
-	                SXSSFCell cell3 = row.createCell(3);
-	            	cell3.setCellStyle(bodyStyle);
-	                cell3.setCellValue(member.getPhone());
-	                SXSSFCell cell4 = row.createCell(4);
-	            	cell4.setCellStyle(bodyStyle);
-	                cell4.setCellValue(member.getMemberRank());
-	                SXSSFCell cell5 = row.createCell(5);
-	            	cell5.setCellStyle(bodyStyle);
-	                cell5.setCellValue(member.getEmail());
-	                
-	            }
-	            
-	            
-	        }while(iterator.hasNext());
-
-	        //lets write the excel data to file now
-	        FileOutputStream fos = new FileOutputStream(fileName);
 	        
-	        response.setContentType("ms-vnd/excel");
+	        Row row = null;
+	        Cell cell = null;
+        	row = sheet.createRow(rowIndex++);	        
+        	cell = row.createCell(0);
+        	cell.setCellStyle(headStyle);
+        	cell.setCellValue("No");
+        	cell = row.createCell(1);
+        	cell.setCellStyle(headStyle);
+        	cell.setCellValue("직원번호");
+        	cell = row.createCell(2);
+        	cell.setCellStyle(headStyle);
+        	cell.setCellValue("이름");
+        	cell = row.createCell(3);
+        	cell.setCellStyle(headStyle);
+        	cell.setCellValue("전화번호");
+        	cell = row.createCell(4);
+        	cell.setCellStyle(headStyle);
+        	cell.setCellValue("직급");
+        	cell = row.createCell(5);
+        	cell.setCellStyle(headStyle);
+        	cell.setCellValue("이메일");
+        	
+        	while(iterator.hasNext()){
+	        	MemberVo member = iterator.next();
+	        	row = sheet.createRow(rowIndex++);
+	           
+	            	cell = row.createCell(0);
+	            	cell.setCellStyle(bodyStyle);
+	            	cell.setCellValue(excelNum++);
+	            	cell = row.createCell(1);
+	            	cell.setCellStyle(bodyStyle);
+	            	cell.setCellValue(member.getNum());
+	            	cell = row.createCell(2);
+	            	cell.setCellStyle(bodyStyle);
+	            	cell.setCellValue(member.getName());
+	            	cell = row.createCell(3);
+	            	cell.setCellStyle(bodyStyle);
+	            	cell.setCellValue(member.getPhone());
+	            	cell = row.createCell(4);
+	            	cell.setCellStyle(bodyStyle);
+	            	cell.setCellValue(member.getMemberRank());
+	            	cell = row.createCell(5);
+	            	cell.setCellStyle(bodyStyle);
+	            	cell.setCellValue(member.getEmail());
+	        }
+        	
+        	sheet.autoSizeColumn(0);
+        	sheet.autoSizeColumn(1);
+        	sheet.autoSizeColumn(2);
+        	sheet.autoSizeColumn(3);
+        	sheet.autoSizeColumn(4);
+        	sheet.autoSizeColumn(5);
+        	
+	        response.setContentType("Application/Msexcel");
 	        response.setHeader("Content-Disposition", "attachment;filename="+fileName);
 	        
-	        workbook.write(fos);
-	        fos.close();
-	        workbook.close();		//HSSFWorkbook, XSSFWorkbook 사용시 사용
-	        //workbook.dispose();		//SXSSFWorkbook 사용시 사용
+	        workbook.write(response.getOutputStream());
+	        workbook.close();
+	        
+	        response.getOutputStream().flush();
+	        response.getOutputStream().close();
 	}
 }
